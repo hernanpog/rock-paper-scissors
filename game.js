5b1a2286dd620fe5c759bdb4d08e0909c0235194
Hernan <hernnpogonza@gmail.com>
2023-10-20 22:06:30 -0300

    Added UI for the game and possibility to play any amount of games

--- game.js
+++ game.js
@@ -1,6 +1,86 @@
 let playerScore = 0;
 let computerScore = 0;
 
+document.querySelector('.js-rock').addEventListener('click', () => {
+    playRound('rock')
+});
+
+document.querySelector('.js-paper').addEventListener('click', () => {
+    playRound('paper');
+});
+
+document.querySelector('.js-scissors').addEventListener('click', () => {
+    playRound('scissors');
+});
+
+function playRound(playerMove) {
+    let computerMove = pickComputerMove();
+    document.querySelector('.js-winner').textContent = '';
+    
+    document.querySelector('.js-playerPick').textContent = `${playerMove}`
+    document.querySelector('.js-computerPick').textContent = `${computerMove}`;
+
+    if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
+            document.querySelector('.js-score')
+            .textContent = `It's a tie!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'paper') {
+            computerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You lose!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'scissors') {
+            playerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You win!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        };
+    }
+    else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
+            playerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You win!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'paper') {
+            document.querySelector('.js-score')
+            .textContent = `It's a tie!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'scissors') {
+            computerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You lose!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+    }
+    else if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
+            computerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You lose!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'paper') {
+            playerScore++;
+            document.querySelector('.js-score')
+            .textContent = `You win!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        }
+        else if (computerMove === 'scissors') {
+            document.querySelector('.js-score')
+            .textContent = `It's a tie!\nYour score: ${playerScore} Computer score: ${computerScore}`
+        };
+    };
+
+    if (playerScore === 5) {
+        document.querySelector('.js-winner').textContent = "Congratulations! You've won this game!"
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        document.querySelector('.js-winner').textContent = "You've lost... Better luck next time."        
+        playerScore = 0;
+        computerScore = 0;
+    }
+};
+
 function pickComputerMove() {
     randomNumber = Math.random();
 
@@ -12,52 +92,5 @@ function pickComputerMove() {
     }
     else {
         return 'scissors';
-    };
-};
-
-function playRound() {
-    let playerMove = prompt('Pick your move: rock, paper, scissors.');
-    playerMove = playerMove.toLowerCase();
-
-    let computerMove = pickComputerMove();
-    
-    if (playerMove === 'rock') {
-        if (computerMove === 'rock') {
-            console.log(`It's a tie! Both chose the same move.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'paper') {
-            computerScore++;
-            console.log(`You lose! Paper beats Rock.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'scissors') {
-            playerScore++;
-            console.log(`You win! Rock beats Scissors.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        };
-    }
-    else if (playerMove === 'paper') {
-        if (computerMove === 'rock') {
-            playerScore++;
-            console.log(`You win! Paper beats Rock.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'paper') {
-            console.log(`It's a tie! Both chose the same move.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'scissors') {
-            computerScore++;
-            console.log(`You lose! Scissors beats Paper.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-    }
-    else if (playerMove === 'scissors') {
-        if (computerMove === 'rock') {
-            computerScore++;
-            console.log(`You lose! Rock beats Scissors.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'paper') {
-            playerScore++;
-            console.log(`You win! Scissors beats Paper.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        }
-        else if (computerMove === 'scissors') {
-            console.log(`It's a tie! Both chose the same move.\nScore: You: ${playerScore} Computer: ${computerScore}`);
-        };
     };
 };
